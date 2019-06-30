874a889bfeb702830f67faa95634c62551f0c878
Yaser Nicaragua <yanv1991@gmail.com>
2019-06-30 16:02:38 -0600

    add docker configuration

--- project/__init__.py
+++ project/__init__.py
@@ -1,17 +1,21 @@
 # services/users/project/__init__.py
 
 
+import os  # new
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
-
+import sys
 
 # instantiate the app
 app = Flask(__name__)
 
+# print(app.config, file=sys.stderr)
+
 api = Api(app)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')  # new
+app_settings = os.getenv('APP_SETTINGS')  # new
+app.config.from_object(app_settings)      # new
 
 
 class UsersPing(Resource):
